db82b99f3d8ff9a396f33f3723f07b3d375989c9
Nick Gaudet <59885900+Nick-Gaudet@users.noreply.github.com>
2021-03-28 18:47:23 -0400

    im in

--- Assignment2/src/sample/Controller.java
+++ Assignment2/src/sample/Controller.java
@@ -2,11 +2,13 @@ package sample;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.Inet4Address;
 import java.net.Socket;
 
 public class Controller {
     public void initialize() throws IOException { // client
-        Socket s = new Socket("localhost", 8080);
+        Inet4Address ip4 = (Inet4Address) Inet4Address.getLocalHost();
+        Socket s = new Socket(ip4, 8080);
 
         PrintWriter p = new PrintWriter(s.getOutputStream());
         p.println("Hey its the client@@@");

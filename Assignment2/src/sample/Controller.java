6d082baf8c30b7882ce439a0c8963cda37ae3246
Nick Gaudet <59885900+Nick-Gaudet@users.noreply.github.com>
2021-03-28 23:27:43 -0400

    hopefully works

--- Assignment2/src/sample/Controller.java
+++ Assignment2/src/sample/Controller.java
@@ -1,19 +1,27 @@
 package sample;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Controller {
     public void initialize() throws IOException { // client
+        Scanner scan = new Scanner(System.in);
         InetAddress ip =  InetAddress.getLocalHost();
         Socket s = new Socket("10.0.0.104", 8080);
 
         PrintWriter p = new PrintWriter(s.getOutputStream());
-        p.println("Hey its the client@@@");
+        System.out.println("Chat: ");
+//        p.println(scan.nextLine());
+        System.out.println(scan.nextLine());
+
+//        p.println("Hey this is client: " + ip.getHostAddress());
+
         p.flush();
+
+
 
     }
 }
--- Assignment2/src/sample/Server.java
+++ Assignment2/src/sample/Server.java
@@ -19,8 +19,15 @@ public class Server {
             System.out.println("client connected!");
             InputStreamReader in = new InputStreamReader(s.getInputStream());
             BufferedReader br = new BufferedReader(in);
-            String str = br.readLine();
-            System.out.println(str);
+            BufferedReader stdIn =
+                    new BufferedReader(
+                            new InputStreamReader(System.in));
+            String str;
+            while((str = stdIn.readLine()) != null){
+                System.out.println(str);
+
+            }
+//            s.close();
         }
 
 
